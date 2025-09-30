0303c08112aced84b1fa5915b0f998f85f9b5050
vasiliy <maximmanjos@gmail.com>
2025-10-01 00:01:50 +0300

    Настроил контейнер с keycloak

--- server/src/main/java/com/drones/skilldrones/config/SecurityConfig.java
+++ server/src/main/java/com/drones/skilldrones/config/SecurityConfig.java
@@ -1,26 +1,25 @@
 package com.drones.skilldrones.config;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-//import static org.springframework.security.config.Customizer.withDefaults;
 
-//@Configuration
+@Configuration
 public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeHttpRequests(authz -> authz
-//                        .requestMatchers("/api/**").permitAll() // Разрешить все API-запросы
-//                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Разрешить Swagger
-//                        .anyRequest().authenticated() // Все остальное пока требует аутентификации
-//                )
-//                .csrf(AbstractHttpConfigurer::disable); // Отключить CSRF для тестирования
-//
-//        return http.build();
-//    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/processing/process-file").hasRole("user") // Требует аутентификации
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> {
+                        })
+                );
+        return http.build();
+    }
 }
-//authenticated()
